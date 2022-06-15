b763101437c5511eac7a0b4843d97c8a48bd3f8e
dulee <duckyounglee@oden.kr>
2022-06-15 14:15:02 +0900

    done

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { LocalAuthGuard } from './auth/guards/local.gurad';
+import { JwtAuthGuard } from './auth/guards/jwt.guard';
+import { AuthService } from './auth/auth.service';
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService,
+  ) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
 
   @Get()
   getHello(): string {
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
+import { AuthService } from './auth.service';
+import { jwtConstants } from './temp-env';
+import { LocalStrategy } from './strategies/local.strategy';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '90s' },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy, LocalStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}

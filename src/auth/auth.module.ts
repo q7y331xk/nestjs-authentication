91d96dfd30e0ae0be6da4d6696e72837c4fc9028
dulee <dulee.dev@gmail.com>
2022-06-22 03:13:51 +0900

    nestjs default

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,8 +4,8 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
-import { jwtConstants } from './temp-env';
 import { LocalStrategy } from './strategies/local.strategy';
+import { jwtConstants } from 'src/secret/jwt';
 
 @Module({
   imports: [
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { jwtConstants } from '../temp-env';
+import { jwtConstants } from 'src/secret/jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
